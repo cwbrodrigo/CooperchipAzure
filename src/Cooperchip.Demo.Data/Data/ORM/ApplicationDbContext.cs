307383f3b119f8c5543f9100fc19e0d77ce567b7
Rodrigo Fleischfresser <cwbrodrigo.87@gmail.com>
2023-07-07 19:04:46 -0300

    Criada connection string e modificado vários arquivos js

--- src/Cooperchip.Demo.Data/Data/ORM/ApplicationDbContext.cs
+++ src/Cooperchip.Demo.Data/Data/ORM/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace Cooperchip.Demo.Data.Data.ORM
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            :base(options){}
+            : base(options) { }
 
         public DbSet<Paciente> Paciente { get; set; }
         public DbSet<EstadoPaciente> EstadoPaciente { get; set; }
--- src/Cooperchip.ItDeveloper.Mvc/Program.cs
+++ src/Cooperchip.ItDeveloper.Mvc/Program.cs
@@ -1,3 +1,6 @@
+using Cooperchip.Demo.Data.Data.ORM;
+using Microsoft.EntityFrameworkCore;
+
 namespace Cooperchip.ItDeveloper.Mvc
 {
     public class Program
@@ -5,6 +8,9 @@ namespace Cooperchip.ItDeveloper.Mvc
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDb")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
