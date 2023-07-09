50a30a80d6d489429e76161182844a79f5ebdf53
Rodrigo Fleischfresser <cwbrodrigo.87@gmail.com>
2023-07-08 23:11:23 -0300

    Adicionada DataAnnotations para as ViewModels e corrigido campo RgDataEmissao na Model e DB

---
+++ src/Cooperchip.Demo.Data/Migrations/20230709020938_MudancaRgDataEmissao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cooperchip.Demo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MudancaRgDataEmissao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EgDataEmissao",
+                table: "Paciente",
+                newName: "RgDataEmissao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RgDataEmissao",
+                table: "Paciente",
+                newName: "EgDataEmissao");
+        }
+    }
+}
